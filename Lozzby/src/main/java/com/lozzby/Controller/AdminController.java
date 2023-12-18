99871c4245eba7918423b8bea5af51777b4b71b6
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-12-18 13:36:19 +0530

    Update AdminController.java

--- Lozzby/src/main/java/com/lozzby/Controller/AdminController.java
+++ Lozzby/src/main/java/com/lozzby/Controller/AdminController.java
@@ -1,7 +1,6 @@
 package com.lozzby.Controller;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.lozzby.Service.CategoryService;
 import com.lozzby.model.Category;
 
