19e09fc0adefad7e4414c928bca2631985f16818
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-12-20 02:51:49 +0530

    Update LoginController.java

--- Lozzby/src/main/java/com/lozzby/Controller/LoginController.java
+++ Lozzby/src/main/java/com/lozzby/Controller/LoginController.java
@@ -2,21 +2,16 @@ package com.lozzby.Controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.lozzby.model.Role;
 import com.lozzby.model.User;
 import com.lozzby.repository.RoleRepo;
 import com.lozzby.repository.UserRepo;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 
